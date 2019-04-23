af9712e5a5de271e9532bb4b560039102928d672
lir <123456@qq.com>
2019-04-23 23:37:12 +0800

    提交

--- src/main/java/com/ServerConfigApplication.java
+++ src/main/java/com/ServerConfigApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerConfigApplication {
 
     public static void main(String[] args) {
+        //初始化
         SpringApplication.run(ServerConfigApplication.class, args);
+
     }
 
 }
